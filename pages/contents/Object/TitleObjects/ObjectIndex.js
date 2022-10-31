8e612524ccb696bcbc293d9cd727540302a568fb
nknig <n.knight,pc0627@gmail.com>
2022-10-31 16:27:00 +0900

    add Object files

--- pages/contents/Object/TitleObjects/ObjectIndex.js
+++ pages/contents/Object/TitleObjects/ObjectIndex.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Text } from "@chakra-ui/react";
+import Box from "./Box";
 import Floor from "./Floor";
+import LightBulb from './Light'
+import OrbitControls from "./OrbitControls";
 
 export default function TitleObjectIndex () {
     return(
@@ -10,14 +13,17 @@ export default function TitleObjectIndex () {
             <Canvas
                 shadows={true}
                 camera={{
-                    position:[-6,7,7]
+                    position:[-6,7,7],
                 }}
                 style={{
                     width: 1700,
                     height: 1000,
                 }}>
                 <ambientLight color={"white"} intensity={0.3} />
-                <Floor/>
+                <LightBulb position={[0, 3, 0]}  />
+                <Box rotateX={3} />
+                <Floor rotateX={13} rotateY={13} rotatZ={13}position={[0, -1, 0]} />
+                <OrbitControls/>
             </Canvas>
         </div>
     )
