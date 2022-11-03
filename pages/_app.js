eecae0dd997dbfdec8e12509dc0151d5e1ed9585
nknig <n.knight,pc0627@gmail.com>
2022-11-03 23:05:04 +0900

    add Analytics component

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-
+import { Analytics } from '@vercel/analytics/react';
 // program entry point
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics/>
+      </>
+  )
 }
 
 export default MyApp
