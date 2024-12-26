eb18829a33ef29b1812a8af1175f6341418a6f01
nknighta <n.knight.pc0627@gmail.com>
2024-12-26 15:58:23 +0900

    feat: 🎸 renewal

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import V1 from './release/V1'
-export default function Index() {
-    return (
-            <ChakraProvider>
-                <V1/>
-            </ChakraProvider>
-    )
-}
